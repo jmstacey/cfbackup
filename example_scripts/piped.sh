cf701bf44bc708f79ae9e6ad0e97fca7644f1e9b
Jon Stacey <jonstacey@jon-staceys-imac.local>
2009-04-11 15:54:33 -0500

    Added example for piped backups and updated documentation.

---
+++ example_scripts/piped.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# In this example, data is piped direcly from the backup to a Cloud Files
+# container, bypassing the intermediate temp directory step
+#
+# This is an example script that you could use in combination with CFBackup.
+# Modify it to suit your needs. Rename to file without an extension if you
+# are going to place in /etc/cron.daily.
+
+cd ~/cfbackup
+CONTAINER=backups
+NOW=$(date +_%b_%d_%y)
+
+# Backup all MySQL databases
+mysqldump -u root -pPASSWORD --all-databases --flush-logs --lock-all-tables | gzip -9 | ruby cfbackup.rb --pipe_data --container $CONTAINER:mysql_all_backup$NOW.sql.gz
+
+# Create main backup archive
+tar -cvf - /home/user /etc /usr/local/nginx | gzip -9 | ruby cfbackup.rb --pipe_data --container $CONTAINER:main_backup$NOW.tar.gz
---
+++ example_scripts/temp_directory.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# In this example, backups are first created and placed in a temporary
+# holding directory. Then, the entire directory is uploaded to 
+# Cloud Files. Finally, the temp directory is cleared in preparation
+# for the next backup.
+#
+# This is an example script that you could use in combination with CFBackup.
+# Modify it to suit your needs. Rename to file without an extension if you
+# are going to place in /etc/cron.daily.
+
+cd ~/cfbackup/temp
+CONTAINER=backups
+NOW=$(date +_%b_%d_%y)
+
+# Dump all MySQL databases
+mysqldump -u root -pPASSWORD --all-databases --flush-logs --lock-all-tables | gzip -9 > mysql_all_backup$NOW.sql.gz
+
+# Create main backup archive
+tar -czvf main_backup$NOW.tar.gz /home/user /etc /usr/local/nginx
+
+cd ~/cfbackup
+ruby cfbackup.rb --local_path temp/ --container $CONTAINER
+rm -f temp/*
