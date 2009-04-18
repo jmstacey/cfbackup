afd0ba31885f68668a5ee532d6fb81329041a869
Jon Stacey <jon@jonsview.com>
2009-04-18 15:38:58 -0500

    The Cloud Files API is now tracked as a submodule from the ruby-cloudfiles repository.

--- cfbackup.rb
+++ cfbackup.rb
@@ -14,7 +14,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'lib/cloudfiles'
+require 'ruby-cloudfiles/lib/cloudfiles'
 require 'OptCFBackup'
 require 'yaml'
 
