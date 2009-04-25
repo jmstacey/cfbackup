98be0022503c6bb337cdb6e9329f90a4a79033c8
Jon Stacey <jon@jonsview.com>
2009-04-25 17:12:34 -0500

    Initial steps of getting tests.

--- test/cfbackup_test.rb
+++ test/cfbackup_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class CfbackupTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+  # Usage: cfbackup.rb --action push|pull|delete options --container CONTAINER
+  
+  # TODO: Mock STDIN to test piping data
+  # context "A push using piped data" do
+  #   setup do
+  #     mock_ARGV = ['--action', 'push', '--pipe_data', '--container', 'test:data.txt', '--config_file', './cfconfig.yml']
+  #     @backup = CFBackup.new(mock_ARGV)
+  #   end
+  #   
+  #   should "return true when piped data successfully sent" do
+  #     assert @backup.run
+  #   end
+  # end # context "A push using piped data"
+  
+  context "A recursive push" do
+    setup do
+      mock_ARGV = ['--action', 'push', '-r', '--container', 'test', '--local_path', 'test/data', '--config_file', 'cfconfig.yml']
+      @backup = CFBackup.new(mock_ARGV)
+    end
+    
+    should "return true when files successfully sent" do
+      assert @backup.run
+    end
+  end # context "A recursive push"
+  
+  # context "A recursive pull" do
+  #   setup do
+  #     mock_ARGV = {""}
+  #     @backup = CFBackup.new(mock_ARGV)
+  #   end
+  #   
+  #   should "return true when files succesfully downloaded" do
+  #     assert @backup.run
+  #   end
+  # end
+  # 
+  # context "A recursive deletion" do
+  #   setup do
+  #     mock_ARGV = {""}
+  #     @backup = CFBackup.new(mock_ARGV)
+  #   end
+  #   
+  #   should "should return true when files existed and were deleted" do
+  #     assert @backup.run
+  #   end
+  # end # context "A recurisve deletion"
+  # 
+  
+  
 end
