86ea3c7d108ca17636bc2c9d0f86de9fd37acf2f
Jon Stacey <jon@jonsview.com>
2009-05-03 14:30:23 -0500

    Removed erroneous .rb from cfbackup call.

--- example_scripts/piped.sh
+++ example_scripts/piped.sh
@@ -7,12 +7,11 @@
 # Modify it to suit your needs. Rename to file without an extension if you
 # are going to place in /etc/cron.daily.
 
-cd ~/cfbackup
 CONTAINER=backups
 NOW=$(date +_%b_%d_%y)
 
 # Backup all MySQL databases
-mysqldump -u root -pPASSWORD --all-databases --flush-logs --lock-all-tables | gzip -9 | cfbackup.rb --action push --pipe_data --container $CONTAINER:mysql_all_backup$NOW.sql.gz
+mysqldump -u root -pPASSWORD --all-databases --flush-logs --lock-all-tables | gzip -9 | cfbackup --action push --pipe_data --container $CONTAINER:mysql_all_backup$NOW.sql.gz
 
 # Create main backup archive
-tar -cvf - /home/user /etc /usr/local/nginx | gzip -9 | cfbackup.rb --action push --pipe_data --container $CONTAINER:main_backup$NOW.tar.gz
+tar -cvf - /home/user /etc /usr/local/nginx | gzip -9 | cfbackup --action push --pipe_data --container $CONTAINER:main_backup$NOW.tar.gz
