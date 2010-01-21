9af5a02452a8f7200b96913664940d74488cd10b
Jon Stacey <jon@jonsview.com>
2010-01-20 23:24:53 -0600

    Adding mocha and cloudfiles dependencies for new tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'mocha'
+require 'cloudfiles'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
